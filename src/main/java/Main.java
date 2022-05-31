18bf0785adfef244da674665ce0f7b90d0e15d2a
chiennguyen7165 <chiennguyen.btw@gmail.com>
2022-05-31 11:54:39 +0700

    5th Commit Connect DB methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,16 @@ public class Main {
     public static void main(String[] args) {
 
         TransportBusiness hello = new TransportBusiness();
-        HashMap<String, Transport> listTrans = hello.createData();
+        DataProvider dp = new DataProvider();
+        dp.ConnectDatabase();
+//        HashMap<String, Transport> listTrans = hello.createData();
 //        Transport newRoadTrans = hello.createTransport();
 //        hello.addTransport(listTrans, newRoadTrans);
 //        hello.showTransport(listTrans);
 //        newRoadTrans.showInfo();
-        String updateID = hello.getTransportID(listTrans);
+//        String updateID = hello.getTransportID(listTrans);
 //        hello.removeTransport(listTrans,removeId);
-        hello.updateTransport(listTrans, updateID);
-        hello.showTransport(listTrans);
+//        hello.updateTransport(listTrans, updateID);
+//        hello.showTransport(listTrans);
     }
 }
--- src/main/java/Module/DataProvider.java
+++ src/main/java/Module/DataProvider.java
@@ -3,4 +3,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DataProvider {
+    public void ConnectDatabase(){
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/transport",
+                            "postgres", "123");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
 }
