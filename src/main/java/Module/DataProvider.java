ec47b1227b0041e5360d3ea5ca1e51affff3b085
chiennguyen7165 <chiennguyen.btw@gmail.com>
2022-06-03 14:24:12 +0700

    6th Commit Update methods connect to database

--- src/main/java/Module/DataProvider.java
+++ src/main/java/Module/DataProvider.java
@@ -1,20 +1,30 @@
 package Module;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DataProvider {
     public void ConnectDatabase(){
-        Connection c = null;
-        try {
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/transport",
-                            "postgres", "123");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName()+": "+e.getMessage());
-            System.exit(0);
+        String url = "jdbc:postgresql://localhost:5432/transport";
+        String user = "postgres";
+        String password = "postgres";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery("SELECT VERSION()")) {
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+
+            Logger lgr = Logger.getLogger(DataProvider.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
         }
-        System.out.println("Opened database successfully");
     }
+
 }
